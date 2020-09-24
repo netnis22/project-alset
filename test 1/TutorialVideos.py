1e00f3e2acbd2554c674d3efb4f88a6e89f410a9
netnis <netnis22@gmail.com>
2020-09-25 02:24:44 +0300

    Tutorial camara 0.2

--- test 1/TutorialVideos.py
+++ test 1/TutorialVideos.py
@@ -1,25 +1,36 @@
 import cv2
 
 
+# this def print indromaisah about the camara
 def infrocam(cap):
-    print('infro from camara')
+    print('infro from camara:')
     print('the HEIGHT ', cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     print('the WIDTH ', cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    print("camara status:", cap.isOpened())
+    print("fps ", cap.get(cv2.CAP_PROP_FPS))
+    print("camara open? ", cap.isOpened())
 
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640, 480))
 
 infrocam(cap)
 
 while cap.isOpened():
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
+    if ret:
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        out.write(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()

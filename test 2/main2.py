403a691ddca76ac36a83d3eed851e4a3b84879c9
netnis <netnis22@gmail.com>
2020-09-25 13:11:01 +0300

    test

--- test 2/main2.py
+++ test 2/main2.py
@@ -1,23 +1,33 @@
 import cv2
 
-cap= cv2.VideoCapture(0)
+
+# this def print indromaisah about the camara
+def infrocam(cap):
+    print('infro from camara:')
+    print('the HEIGHT ', cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print('the WIDTH ', cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print("fps ", cap.get(cv2.CAP_PROP_FPS))
+    print("camara open? ", cap.isOpened())
+
+
+cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640, 480))
 
-print(cap.isOpened())
-while(cap.isOpened()):
-    ret,frame = cap.read()
-    if ret == True:
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# print indromaisah about the camara
+infrocam(cap)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
 
         out.write(frame)
 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
 
-        gray =cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame',gray)
-
-        if cv2.waitKey(1) & 0xff == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break

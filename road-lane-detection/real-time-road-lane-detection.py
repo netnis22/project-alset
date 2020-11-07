d331d4a6bcbbc23809228803f4c2dcc4002d54e8
netnis <netnis22@gmail.com>
2020-11-07 15:52:35 +0200

    real-time-road-lane-detection work

--- road-lane-detection/real-time-road-lane-detection.py
+++ road-lane-detection/real-time-road-lane-detection.py
@@ -5,6 +5,32 @@ import matplotlib.pyplot as plt
 import datetime
 
 
+# פונקציות שאני לא מישתמש
+def average_slope_intercept1(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+
+        # It will fit the polynomial and the intercept and slope
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+
+    if left_fit:
+        left_fit_average = np.average(left_fit, axis=0)
+        print(left_fit_average, 'left')
+        left_line = create_coordinates(image, left_fit_average)
+    if right_fit:
+        right_fit_average = np.average(right_fit, axis=0)
+        print(right_fit_average, 'right')
+        right_line = create_coordinates(image, right_fit_average)
+
+
 # Prints camera information
 def infrocamPrint(cap):
     print('infro from camara:')
@@ -49,8 +75,8 @@ def showImages(frame, mask, edges, roi, res):
     cv.imshow("video", frame)
     cv.imshow("mask", mask)
     cv.imshow('Edges', edges)
-    cv.imshow("res", res)
     cv.imshow("roi", roi)
+    cv.imshow("res", res)
 
 
 # Checks if esc is pressed and if esc is pressed returns False otherwise returns True
@@ -64,8 +90,8 @@ def breakLoop():
 
 def region_of_interest(edges):
     height = edges.shape[0]
-    polygons1 = np.array([[(0, height), (640, height), (0, 250)]])
-    polygons2 = np.array([[(0, 250), (640, height), (640, 250)]])
+    polygons1 = np.array([[(180, height), (640, height), (180, 250)]])
+    polygons2 = np.array([[(180, 250), (640, height), (640, 250)]])
 
     mask = np.zeros_like(edges)
     cv.fillPoly(mask, polygons1, 255)
@@ -75,7 +101,7 @@ def region_of_interest(edges):
     return masked_image
 
 
-#def create_coordinates(image, line_parameters):
+def create_coordinates(image, line_parameters):
     slope, intercept = line_parameters
     y1 = image.shape[0]
     y2 = int(y1 * (3 / 5))
@@ -84,29 +110,48 @@ def region_of_interest(edges):
     return np.array([x1, y1, x2, y2])
 
 
-#def average_slope_intercept(image, lines):
+def average_slope_intercept(image, lines):
+    left =False
+    right = False
     left_fit = []
     right_fit = []
-    for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+    if left_fit:
+        left_fit_average = np.average(left_fit, axis=0)
+        #print(left_fit_average, 'left')
+        #print('left')
+        left = True
+        left_line = create_coordinates(image, left_fit_average)
+    else:
+        left = False
+    if right_fit:
+        right_fit_average = np.average(right_fit, axis=0)
+        #print(right_fit_average, 'right')
+        #print('right')
+        right = True
+        right_line = create_coordinates(image, right_fit_average)
+    else:
+        right = False
+    if(right & left):
+        print('ok')
+    elif (right & (left == False)):
+        print('left')
+    elif ((right == False) & left):
+        print('right')
+    else:
+        print('stop')
 
-        # It will fit the polynomial and the intercept and slope
-        parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-        if slope < 0:
-            left_fit.append((slope, intercept))
-        else:
-            right_fit.append((slope, intercept))
 
-    left_fit_average = np.average(left_fit, axis=0)
-    right_fit_average = np.average(right_fit, axis=0)
-    left_line = create_coordinates(image, left_fit_average)
-    right_line = create_coordinates(image, right_fit_average)
-    return np.array([left_line, right_line])
-
-
-#def display_lines(image, lines):
+def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for x1, y1, x2, y2 in lines:
@@ -125,13 +170,13 @@ def main():
         mask = makeMask(frame)  # Activates the function makemask
         edges = cv.Canny(mask, 0, 400)  # Prepares an image only at the edges of the mask
         roi = region_of_interest(edges)
+        debug = region_of_interest(frame)
+        cv.imshow('debug', debug)
 
-        #lines = cv.HoughLinesP(roi, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-        #averaged_lines = average_slope_intercept(frame, lines)
-        #line_image = display_lines(frame, averaged_lines)
-        #res = cv.addWeighted(frame, 0.8, line_image, 1, 1)
-        res =frame
-
+        lines = cv.HoughLinesP(roi, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+        averaged_lines = average_slope_intercept(edges, lines)
+        line_image = display_lines(frame, averaged_lines)
+        res = cv.addWeighted(frame, 0.8, line_image, 1, 1)
         showImages(frame, mask, edges, roi, res)  # Activates the function showImages
 
         flag = breakLoop()  # Activates the function showImages breakLoop

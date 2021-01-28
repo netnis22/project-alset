f1b4d242c92f272b373047e0ceb1f2e19d97e35d
netnis <netnis22@gmail.com>
2021-01-28 10:06:18 +0200

    add file

---
+++ Arduino and rp3/arduino_serial1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import time
+import serial
+
+
+def main1():
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    # ser.flush()
+    # ser.reset_input_buffer()
+    # ser.reset_output_buffer()
+
+    while True:
+        # buff = input('> ').encode()
+        # ser.write(buff)
+
+        # print(f'in_waiting = {ser.in_waiting}')
+        while ser.in_waiting > 0:
+            print(f'in_waiting = {ser.in_waiting}')
+            rd = ser.read().decode()
+            print(rd)
+
+        time.sleep(0.1)
+
+
+def main():
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    # ser.flush()
+    while True:
+        buff = input('> ').encode()
+        ser.write(buff)
+        time.sleep(0.1)
+        while ser.in_waiting > 0:
+            line = ser.read().decode()
+            print(line)
+    time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
